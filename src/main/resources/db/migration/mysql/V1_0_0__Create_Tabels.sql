89c67f9a6832d10b26617884a756c10b5255f4e1
SzTOlivér <ay5ayc@inf.elte.hu>
2024-02-07 18:53:20 +0100

    little changes of user table

--- src/main/resources/db/migration/mysql/V1_0_0__Create_Tabels.sql
+++ src/main/resources/db/migration/mysql/V1_0_0__Create_Tabels.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
                         id INT PRIMARY KEY AUTO_INCREMENT,
-                        username VARCHAR(255) NOT NULL,
+                        first_name VARCHAR(255) NOT NULL,
+                        last_name VARCHAR(255) NOT NULL,
                         email VARCHAR(255) NOT NULL,
                         password VARCHAR(255) NOT NULL,
                         phone_number VARCHAR(255) NOT NULL,
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS flights (

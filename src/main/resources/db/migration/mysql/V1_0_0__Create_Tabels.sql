48ca98c3ebdce9317dbb2b91b0cf144cd6bd1090
SzTOlivér <ay5ayc@inf.elte.hu>
2024-02-07 18:37:30 +0100

    App settings changed and databases created with flyway

---
+++ src/main/resources/db/migration/mysql/V1_0_0__Create_Tabels.sql
@@ -0,0 +1,80 @@
+CREATE TABLE IF NOT EXISTS users (
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        username VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        phone_number VARCHAR(255) NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS flights (
+                         id INT PRIMARY KEY AUTO_INCREMENT,
+                         airline VARCHAR(255) NOT NULL,
+                         origin VARCHAR(255) NOT NULL,
+                         destination VARCHAR(255) NOT NULL,
+                         departure_date DATE NOT NULL,
+                         duration INT NOT NULL,
+                         created_at BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS seats (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       seat_number VARCHAR(10),
+                       seat_type VARCHAR(50),
+                       price INT NOT NULL,
+                       availability BOOLEAN,
+                       flight_id INT,
+                       FOREIGN KEY (flight_id) REFERENCES flights(id)
+);
+
+CREATE TABLE IF NOT EXISTS hotels (
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        name VARCHAR(255) NOT NULL,
+                        location VARCHAR(255) NOT NULL,
+                        check_in_date BIGINT NOT NULL,
+                        check_out_date BIGINT NOT NULL,
+                        created_at BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS rooms (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       room_type VARCHAR(50),
+                       capacity INT,
+                       price_per_night INT NOT NULL,
+                       availability BOOLEAN,
+                        hotel_id INT,
+                       FOREIGN KEY (hotel_id) REFERENCES hotels(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS rental_cars (
+                             id INT PRIMARY KEY AUTO_INCREMENT,
+                             brand VARCHAR(255) NOT NULL,
+                             model VARCHAR(255) NOT NULL,
+                             location VARCHAR(255) NOT NULL,
+                             pickup_date DATE NOT NULL,
+                             return_date DATE NOT NULL,
+                             total_price INT NOT NULL,
+                             created_at BIGINT NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS bookings (
+                            id INT PRIMARY KEY AUTO_INCREMENT,
+                            booking_type VARCHAR(255) NOT NULL,
+                            booking_id INT NOT NULL,
+                            total_price INT NOT NULL,
+                            booking_date BIGINT NOT NULL,
+                            user_id INT NOT NULL,
+                            FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        review_type VARCHAR(255) NOT NULL,
+                        review_id INT NOT NULL,
+                        rating INT NOT NULL,
+                        comment VARCHAR(2000),
+                        review_date BIGINT NOT NULL,
+                        user_id INT NOT NULL,
+                        FOREIGN KEY (user_id) REFERENCES users(id)
+);
---
+++ src/main/resources/db/migration/mysql/V1_0_1__addItems.sql
@@ -0,0 +1,45 @@
+INSERT INTO flights (airline, origin, destination, departure_date, duration, created_at)
+VALUES
+    ('Delta Airlines', 'New York', 'Los Angeles', '2023-11-15', 360, UNIX_TIMESTAMP()),
+    ('American Airlines', 'Chicago', 'Miami', '2023-11-20', 180, UNIX_TIMESTAMP()),
+    ('United Airlines', 'San Francisco', 'Seattle', '2023-11-25', 120, UNIX_TIMESTAMP()),
+    ('British Airways', 'London', 'Paris', '2023-12-01', 90, UNIX_TIMESTAMP());
+
+INSERT INTO seats (seat_number, seat_type, price, availability, flight_id)
+VALUES
+    ('A1', 'Economy', 100, TRUE, 1),
+    ('A2', 'Economy', 100, TRUE, 1),
+    ('B1', 'Business', 200, TRUE, 1),
+    ('B2', 'Business', 200, TRUE, 1),
+    ('C1', 'Economy', 120, TRUE, 2),
+    ('C2', 'Economy', 120, TRUE, 2),
+    ('D1', 'Business', 220, TRUE, 2),
+    ('D2', 'Business', 220, TRUE, 2);
+
+
+INSERT INTO hotels (name, location, check_in_date, check_out_date, created_at)
+VALUES
+    ('Hilton', 'New York', UNIX_TIMESTAMP('2023-11-15'), UNIX_TIMESTAMP('2023-11-20'), UNIX_TIMESTAMP()),
+    ('Marriott', 'Chicago', UNIX_TIMESTAMP('2023-11-20'), UNIX_TIMESTAMP('2023-11-25'), UNIX_TIMESTAMP()),
+    ('Sheraton', 'Los Angeles', UNIX_TIMESTAMP('2023-11-25'), UNIX_TIMESTAMP('2023-11-30'), UNIX_TIMESTAMP()),
+    ('Radisson', 'San Francisco', UNIX_TIMESTAMP('2023-12-01'), UNIX_TIMESTAMP('2023-12-05'), UNIX_TIMESTAMP());
+
+
+INSERT INTO rooms (room_type, capacity, price_per_night, availability, hotel_id)
+VALUES
+    ('Single', 1, 100, TRUE, 1),
+    ('Double', 2, 150, TRUE, 1),
+    ('Suite', 4, 250, TRUE, 1),
+    ('Single', 1, 90, TRUE, 2),
+    ('Double', 2, 130, TRUE, 2),
+    ('Suite', 4, 220, TRUE, 2);
+
+INSERT INTO rental_cars (brand, model, location, pickup_date, return_date, total_price, created_at)
+VALUES
+    ('Toyota', 'Camry', 'New York', '2023-11-15', '2023-11-20', 300, UNIX_TIMESTAMP()),
+    ('Ford', 'Mustang', 'Chicago', '2023-11-20', '2023-11-25', 400, UNIX_TIMESTAMP()),
+    ('Chevrolet', 'Impala', 'Los Angeles', '2023-11-25', '2023-11-30', 350, UNIX_TIMESTAMP()),
+    ('Honda', 'Accord', 'San Francisco', '2023-12-01', '2023-12-05', 380, UNIX_TIMESTAMP());
+
+
+

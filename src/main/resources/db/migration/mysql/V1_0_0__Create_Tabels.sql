fec044db9be1b204097c5fb70be60cb5af00bc29
SzTOlivér <ay5ayc@inf.elte.hu>
2024-02-08 16:04:06 +0100

    email and phone number parameter was changed

--- src/main/resources/db/migration/mysql/V1_0_0__Create_Tabels.sql
+++ src/main/resources/db/migration/mysql/V1_0_0__Create_Tabels.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS users (
                         first_name VARCHAR(255) NOT NULL,
                         last_name VARCHAR(255) NOT NULL,
                         username VARCHAR(255) NOT NULL,
-                        email VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL UNIQUE,
                         password VARCHAR(255) NOT NULL,
-                        phone_number VARCHAR(255) NOT NULL
+                        phone_number VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS flights (

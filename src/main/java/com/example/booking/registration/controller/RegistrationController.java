848b38d1d70ad6f2f0dffdbbcac31ed2762b8002
SzTOlivér <ay5ayc@inf.elte.hu>
2024-02-09 16:59:04 +0100

    GET users removed and Registrations todo removed as well

--- src/main/java/com/example/booking/registration/controller/RegistrationController.java
+++ src/main/java/com/example/booking/registration/controller/RegistrationController.java
@@ -6,7 +6,6 @@ import com.example.booking.registration.services.RegistrationService;
 import com.example.booking.user.models.User;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +19,7 @@ public class RegistrationController {
   private RegistrationService registrationService;
 
   @PostMapping("/register")
-  public ResponseEntity<?> register(@RequestBody @Valid RegistrationDTO rdto) {//todo a ? jelet kiszedni
+  public ResponseEntity<RegistrationResponseDTO> register(@RequestBody @Valid RegistrationDTO rdto) {
     User user = registrationService.register(rdto);
     return ResponseEntity.status(201).body(new RegistrationResponseDTO(
             user.getId(), user.getUsername(), user.getEmail(), user.getPhoneNumber()));

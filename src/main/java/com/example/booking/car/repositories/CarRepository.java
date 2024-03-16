ab75d8f9bd1913efd162bd70728f41e504c47824
SzTOlivér <ay5ayc@inf.elte.hu>
2024-03-16 21:09:19 +0100

    sql query's is changed

--- src/main/java/com/example/booking/car/repositories/CarRepository.java
+++ src/main/java/com/example/booking/car/repositories/CarRepository.java
@@ -12,9 +12,9 @@ public interface CarRepository extends CrudRepository<Car, Integer> {
   @Query(value = "SELECT * FROM cars " +
           "WHERE pick_up_location = :#{#pickUpLocation} " +
           "AND drop_off_location = :#{#pickUpLocation} " +
-          "AND (:carType is null or car_type = :carType) " +
-          "AND (:capacity is null or capacity = :capacity) " +
-          "AND (:transmissionType is null or transmission_type = :transmissionType)", nativeQuery = true)
+          "AND (:carType is null or :carType = '' or car_type = :carType) " +
+          "AND (:capacity is null or :capacity = '' or capacity = :capacity) " +
+          "AND (:transmissionType is null or :transmissionType = '' or transmission_type = :transmissionType)", nativeQuery = true)
   List<Car> findSameDropOffLocationCar(@Param("pickUpLocation") String pickUpLocation,
                                        @Param("carType") String carType,
                                        @Param("capacity") Integer capacity,
@@ -23,9 +23,9 @@ public interface CarRepository extends CrudRepository<Car, Integer> {
   @Query(value = "SELECT * FROM cars " +
           "WHERE pick_up_location = :#{#pickUpLocation} " +
           "AND drop_off_location = :#{#dropOffLocation} " +
-          "AND (:carType is null or car_type = :carType) " +
-          "AND (:capacity is null or capacity = :capacity) " +
-          "AND (:transmissionType is null or transmission_type = :transmissionType)", nativeQuery = true)
+          "AND (:carType is null or :carType = '' or car_type = :carType) " +
+          "AND (:capacity is null or :capacity = '' or capacity = :capacity) " +
+          "AND (:transmissionType is null or :transmissionType = '' or transmission_type = :transmissionType)", nativeQuery = true)
   List<Car> findByPickUpLocationAndDropOffLocation(@Param("pickUpLocation") String pickUpLocation,
                                                    @Param("dropOffLocation") String dropOffLocation,
                                                    @Param("carType") String carType,

0bd320bcb78c9e6b5a3865243e915ffbd248e5c0
SzTOlivér <ay5ayc@inf.elte.hu>
2024-04-09 11:40:05 +1000

    Review modal is created with style

--- src/main/frontend/src/ReviewComponent/Review.js
+++ src/main/frontend/src/ReviewComponent/Review.js
@@ -1,18 +1,40 @@
-const Review = ({ reviews }) => {
+import React from "react";
+
+const Review = ({reviews, toggleReviewsModal, showReviewsModal}) => {
+
     return (
-        <div>
-            {reviews.map((review, index) => (
-                <div key={index}>
-                    <div>
-                        <strong>Username:</strong> {review.username} - <strong>Review Date:</strong> {review.reviewDate}
+        showReviewsModal && (
+            <div className="modal show blur-exclude review" tabIndex="-1" role="dialog" style={{display: 'block'}}>
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Reviews</h5>
+                            <button type="button" className="close" onClick={toggleReviewsModal}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {reviews.map((review, index) => (
+                                <div key={index}>
+                                    <div>
+                                        <strong>Username:</strong> {review.username} - <strong>Review
+                                        Date:</strong> {review.reviewDate}
+                                    </div>
+                                    <div>
+                                        <strong>Comment:</strong> {review.comment}
+                                    </div>
+                                    <hr/>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" onClick={toggleReviewsModal}>Close
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                        <strong>Comment:</strong> {review.comment}
-                    </div>
-                    <hr/>
                 </div>
-            ))}
-        </div>
+            </div>
+        )
     );
 };
 export default Review;

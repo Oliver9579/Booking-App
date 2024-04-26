a19766c5910ae46f17e342422c432b2b7fc498d0
SzTOlivér <ay5ayc@inf.elte.hu>
2024-04-26 16:24:10 +0200

    Now the user can write reviews for one way flights, and the user can see the seats as well

--- src/main/frontend/src/BookingComponent/BookingCards/BookingOneWayFlightCard.js
+++ src/main/frontend/src/BookingComponent/BookingCards/BookingOneWayFlightCard.js
@@ -3,6 +3,7 @@ import {FormatDate} from "./FormatDate";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEuroSign} from "@fortawesome/free-solid-svg-icons";
 import Review from "../../ReviewComponent/Review";
+import AddReview from "../../ReviewComponent/AddReview";
 
 const BookingOneWayFlightCard = ({booking}) => {
 
@@ -10,6 +11,12 @@ const BookingOneWayFlightCard = ({booking}) => {
     const landingTime = new Date(departureTime.getTime() + booking.flight.duration * 60 * 1000);
 
     const [showReviewsModal, setShowReviewsModal] = useState(false);
+    const [showSeats, setShowSeats] = useState(false);
+    const [reviews, setReviews] = useState(booking.flight.reviews);
+
+    const handleNewReview = (newReview) => {
+        setReviews(prevReviews => [...prevReviews, newReview]);
+    };
 
     const toggleReviewsModal = () => {
         setShowReviewsModal(!showReviewsModal);
@@ -29,7 +36,7 @@ const BookingOneWayFlightCard = ({booking}) => {
                 {booking.flight.destinationAirportCode} {booking.flight.destination}</h4>
             <div className="blur-include"
                  style={{paddingBottom: '15px'}}>{FormatDate(departureTime)} - {FormatDate(landingTime)}</div>
-            <div className="booking-card blur-include">
+            <div className="booking-card blur-include" style={{height: '130px'}}>
                 <div className="row" style={{width: '100%', height: '100%'}}>
                     <div className="col-2" style={{height: '100%', display: "flex"}}>
                         <img src={require(`../../FlightsComponent/img/${booking.flight.img}`)}
@@ -50,26 +57,42 @@ const BookingOneWayFlightCard = ({booking}) => {
                             {FormatDate(departureTime)} - {FormatDate(landingTime)}</div>
                         <div>
                             {new Date(landingTime) < new Date() ?
-                                <div style={{fontWeight: "350", fontSize: '90%'}}>Completed</div>
+                                <div style={{fontWeight: "350", fontSize: '90%', paddingBottom: '5px'}}>Completed</div>
                                 :
-                                <div style={{fontWeight: "350", fontSize: '90%'}}>Active</div>
+                                <div style={{fontWeight: "350", fontSize: '90%', paddingBottom: '5px'}}>Active</div>
                             }
+                        </div>
+                        {showSeats && (
+                            <div className="show-seats">
+                                {booking.flight.seats.map((seat, index) => (
+                                    <div style={{fontWeight: "350", fontSize: '90%'}}
+                                         key={index}>{`Number ${seat.seatNumber}
+                                (${seat.seatType.toLowerCase()})`}</div>
+                                ))}
+                            </div>
+                        )}
+                        <div style={{fontWeight: "550", fontSize: '90%', width: '8%', textDecoration: "underline"}}
+                             onMouseEnter={() => setShowSeats(true)}
+                             onMouseLeave={() => setShowSeats(false)}>
+                            {booking.flight.seats.length === 1 ? 'Seat' : 'Seats'}
                         </div>
                     </div>
                     <div className="col-2" style={{textAlign: 'right', fontSize: '20px'}}>
                         <strong><FontAwesomeIcon icon={faEuroSign}/> {booking.totalPrice}</strong>
                         <button className="btn btn-primary reviews-button"
-                                style={{height: '40%', marginLeft: '20%'}}
+                                style={{height: '30px', marginLeft: '10%'}}
                                 type="submit" onClick={toggleReviewsModal}><span></span>Reviews
                         </button>
                         <div className="d-inline"
-                             style={{padding: '2px', fontSize: '17px'}}>({booking.flight.reviews.length})
+                             style={{padding: '2px', fontSize: '17px'}}>({reviews.length})
                         </div>
                     </div>
                 </div>
             </div>
-            <Review reviews={booking.flight.reviews} toggleReviewsModal={toggleReviewsModal}
+            <Review reviews={reviews} toggleReviewsModal={toggleReviewsModal}
                     showReviewsModal={showReviewsModal}></Review>
+
+            <AddReview bookedEntity={booking.flight} onNewReview={handleNewReview}></AddReview>
         </div>
     )
 }
---
+++ src/main/frontend/src/ReviewComponent/AddReview.js
@@ -0,0 +1,54 @@
+import "./AddReview.css"
+import {useState} from "react";
+import axios from "axios";
+
+const AddReview = ({bookedEntity, onNewReview}) => {
+
+    const [comment, setComment] = useState('');
+
+
+    const handleAddNewReview = () => {
+        const requestBody = {
+            comment: comment,
+        };
+
+        try {
+            const token = localStorage.getItem("token");
+            const config = {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            };
+            const url = 'http://localhost:3000/api/reviews/flight/' + bookedEntity.id
+            axios.post(
+                url,
+                requestBody,
+                config
+            ).then((response) => {
+                setComment('');
+                onNewReview(response.data);
+            });
+        } catch (error) {
+            alert('Error while add review:');
+        }
+    };
+    const handleInputChange = (event) => {
+        setComment(event.target.value);
+    };
+
+    return (
+        <div className="add-review-card">
+            <div className="row">
+                <div className="col-10" style={{paddingRight: 0}}>
+                    <input className="comment" type="text" name="comment" placeholder="Add review..." value={comment}
+                           onChange={handleInputChange}></input>
+                </div>
+                <div className="col-2" style={{paddingLeft: 0, textAlign: "right"}}>
+                    <input className="btn btn-primary add-review-button" type="submit" value="Submit"
+                           disabled={comment.trim() === ''} onClick={handleAddNewReview}></input>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AddReview
